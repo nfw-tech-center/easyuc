e0c104ad4978b67a8bd70b8eb2cf674546500949
Abel <zxz054321@foxmail.com>
2018-08-15 17:08:30 +0800

    config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,5 +7,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
+
+        $this->publishes([
+            __DIR__ . '/config.php' => config_path('easyuc.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/config.php', 'easyuc'
+        );
     }
 }
