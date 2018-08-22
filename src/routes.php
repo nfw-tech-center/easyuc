46c6ad76776dcb04b6dea3d5e48d97818a47898e
Abel <zxz054321@foxmail.com>
2018-08-22 11:19:10 +0800

    use middleware group

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => config('easyuc.route.prefix')], function () {
-    Route::get('uc/obtain-token', 'Abel\EasyUC\Controllers\OAuthController@obtainToken');
-    Route::post('uc/users/list', 'Abel\EasyUC\Controllers\UserController@listUser');
-    Route::post('uc/users/add', 'Abel\EasyUC\Controllers\UserController@addUser');
-    Route::post('uc/users/delete', 'Abel\EasyUC\Controllers\UserController@destoryUser');
+
+    Route::group(['middleware' => 'web'], function () {
+        Route::get('uc/obtain-token', 'Abel\EasyUC\Controllers\OAuthController@obtainToken');
+    });
+
+    Route::group(['middleware' => 'api'], function () {
+        Route::post('uc/users/list', 'Abel\EasyUC\Controllers\UserController@listUser');
+        Route::post('uc/users/add', 'Abel\EasyUC\Controllers\UserController@addUser');
+        Route::post('uc/users/delete', 'Abel\EasyUC\Controllers\UserController@destoryUser');
+    });
+
 });
