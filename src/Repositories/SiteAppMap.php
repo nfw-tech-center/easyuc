35f53b588cdb913290d0f804cceb0a688ca9631a
Abel <zxz054321@foxmail.com>
2019-03-29 09:03:49 +0800

    sites with app permission

--- src/Repositories/SiteAppMap.php
+++ src/Repositories/SiteAppMap.php
@@ -33,12 +33,16 @@ class SiteAppMap
     }
 
     /**
-     * 返回用户开启了本应用权限的站点
+     * 返回用户开启了指定应用权限的站点
      */
     public function sitesWithAppPermission(int $app): Collection
     {
-        return collect($this->data)->reject(function ($site) use ($app) {
-            return !collect($site->app_list)->pluck('id')->flip()->has($app);
-        });
+        return collect($this->data)
+            // 排除未开启指定应用权限的站点
+            ->reject(function ($site) use ($app) {
+                return !collect($site->app_list)->pluck('id')->flip()->has($app);
+            })
+            // 去除数组数字键
+            ->values();
     }
 }
