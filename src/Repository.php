f6ba296e23102f298e5b702c06adb9f0db3db420
Abel <zxz054321@foxmail.com>
2019-07-10 15:04:55 +0800

    refactor

--- src/Repository.php
+++ src/Repository.php
@@ -29,8 +29,8 @@ class Repository
      */
     public function authorized(): bool
     {
-        // 用户旗下的站点中，至少要有1个开启了本应用
-        return count($this->sites->data);
+        // 管理员，或站点用户旗下的站点有开启了本应用的
+        return $this->user->group < 10 || count($this->sites->data);
     }
 
     public function __get($name)
